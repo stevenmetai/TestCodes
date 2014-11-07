f40dc11a1e45242c4f192694642cc6d69083f911
StevenTai <stevenmetai48@gmail.com>
2014-11-07 14:47:34 +0800

    Add mergesort

---
+++ Sort/src/MergeSort.java
@@ -0,0 +1,15 @@
+
+public class MergeSort extends Sort{
+
+	public MergeSort(int[] num) {
+		super(num);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void doSort() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
